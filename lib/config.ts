dbb4eaa51fc18c2d4f3fb4fce6d6f930021e52f1
mehul-jain_nextiva <mehul.jain@nextiva.com>
2025-02-07 04:07:44 +0530

    implemented upstash redis and rate limiting concept

--- lib/config.ts
+++ lib/config.ts
@@ -8,6 +8,10 @@ const config = {
         privateKey: process.env.IMAGE_KIT_PRIVATE_KEY!,
       },
       databaseUrl: process.env.DATABASE_URL!,
+      upstash:{
+        redisUrl:process.env.UPSTASH_REDIS_URL!,
+        redisToken:process.env.UPSTASH_REDIS_TOKEN!
+      }
     //   upstash: {
     //     redisUrl: process.env.UPSTASH_REDIS_URL!,
     //     redisToken: process.env.UPSTASH_REDIS_TOKEN!,
